02e3e4c240a8bb8379e629204d1a39f6660ce74e
Johnny Pacia <johnny.pacia@gmail.com>
2016-01-20 21:19:25 -0500

    adds rails app with migrations seeds database and sessions

---
+++ sound_shaper_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController <ApplicationController
+
+	def new
+		if session[:user_id]
+			redirect_to sounds_path
+		end
+	end
+
+	#/
+	def create
+		user = User.find_by({email: params['/'][:email]})
+		if user && user.authenticate(params['/'][:password])
+			session[:user_id] = user.id
+			flash[:notice] ="That's the right on, Man."
+			redirect_to sounds_path(@sounds)
+		else
+			flash[:notice]="Either your e-mail or password didn't work. Please try again."
+			render :new
+		end
+	end
+
+	#/logout
+	def destroy
+		session[:user_id] = nil
+		flash[:notice] = "Logged Out Nowabouts."
+		redirect_to "/"
+	end
+
+end
